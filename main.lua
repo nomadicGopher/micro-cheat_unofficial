8414460c03e9cb18af96660117f08401104a242c
nomadicGopher <11096910+nomadicGopher@users.noreply.github.com>
2025-06-13 19:23:24 -0700

    Fix regex escape & broken path
    - fix broken file path (assuming micro-cheat is an old plugin path based on the repo name)
    - double backslash escape character per lua syntax
    - remove basic comments

--- main.lua
+++ main.lua
@@ -7,23 +7,10 @@
 
 local micro = import("micro")
 local config = import("micro/config")
-local os = import("os")
 
-local cheatdir = config.ConfigDir.."/plug/micro-cheat/cheatsheets/"
+local cheatdir = config.ConfigDir.."/plug/cheat/cheatsheets/"
 
-function getSuffix(filename)
-	-- return last suffix without dot. Example: "tero.foo.bar.sass" -> "sass"
-	return filename:match("%.(%w+)$")
-end
-
-function fileExists(filename)
-	-- return true if "filename" exists
-	local _, err = os.Stat(filename)
-	return not err
-end
-	
 function init()
-	-- runs once when micro starts
 	config.MakeCommand("cheat", cheatCommand, config.NoComplete)
 	config.TryBindKey("F1", "command:cheat", false)
 end
@@ -31,37 +18,21 @@ end
 function cheatCommand(bp)
 	local filename = bp.Buf:GetName()
 	local filetype = bp.Buf:FileType()
-	local filesuffix = getSuffix(filename)
 
 	if "Vagrantfile" == filename or "Dockerfile" == filename then
 		filetype = filename:lower()
 		micro.InfoBar():Message("Cheatsheet by file name: "..filename)
--- Disabled org-mode support, because literal dot (backslash dot) escape causes error
--- in the micro shipped by latest Kali
---	elseif string.find(filename, "\.org$") then -- not detected by micro
---		filetype = "org"
---		micro.InfoBar():Message("Cheatsheet by file suffix: "..filename)
+	elseif string.find(filename, "\\.org$") then
+		filetype = "org"
+		micro.InfoBar():Message("Cheatsheet by file suffix: "..filename)
 	elseif "unknown" ~= filetype then
-		-- micro hopefully detected the filetype, the typical case
 		micro.InfoBar():Message("Cheatsheet by file type: "..filetype)
-	elseif nil ~= filesuffix then
-		filetype = filesuffix
-		micro.InfoBar():Message("Cheatsheet by file suffix: "..filetype)
-	--else
-	--	micro.InfoBar():Message("Cheatsheet not found for type '"..filetype.."', filename '"..filename.."'")
-	--	return
-	end
-
-	local cheatsheet = cheatdir .. filetype .. ".md"
-
-	if not fileExists(cheatsheet) then
-		micro.InfoBar():Message("Cheatsheet for \""..filetype.."\" does not exist. Contribute to https://github.com/terokarvinen/micro-cheat")
+	else
+		micro.InfoBar():Message("Cheatsheet not found for type '"..filetype.."', filename '"..filename.."'")
+		return
 	end
 	
-	-- local cmd = "tab " .. cheatdir .. filetype .. ".md"
-	local cmd = "tab " .. cheatsheet
+	local cmd = "tab " .. cheatdir .. filetype .. ".md"
 	bp:HandleCommand(cmd)
-	
-	-- bp:HandleCommand("setlocal readonly true") -- user changes would be overwritten my micro-cheat updates
 
 end
